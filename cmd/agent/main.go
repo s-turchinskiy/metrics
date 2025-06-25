3fccdfb700abab14ed40098af7808a3fd6e11345
Stanislav Turchinskiy <s.turchinskiy@yandex.ru>
2025-06-25 10:06:12 +0300

    iter2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,160 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"runtime"
+	"time"
+)
+
+const (
+	pollInterval   = 2
+	reportInterval = 10
+)
+
+var (
+	metricsNames = []string{"Alloc", "BuckHashSys", "Frees", "GCCPUFraction", "GCSys", "HeapAlloc", "HeapIdle", "HeapInuse",
+		"HeapObjects", "HeapReleased", "HeapSys", "LastGC", "Lookups", "MCacheInuse", "MCacheSys", "MSpanInuse", "MSpanSys",
+		"Mallocs", "NextGC", "NumForcedGC", "NumGC", "OtherSys", "PauseTotalNs", "StackInuse", "StackSys", "Sys", "TotalAlloc"}
+)
+
+type MetricsUpdaterReporting interface {
+	UpdateMetrics() error
+	ReportMetrics() error
+}
+
+type MetricsHandler struct {
+	storage MetricsUpdaterReporting
+}
+
+func main() {
+
+	metricsHandler := &MetricsHandler{
+		storage: &MetricsStorage{
+			Gauge:         make(map[string]float64),
+			Counter:       make(map[string]int64),
+			ServerAddress: "http://localhost:8080",
+		},
+	}
+
+	errors := make(chan error, 0)
+
+	go func() {
+
+		for {
+
+			err := metricsHandler.storage.UpdateMetrics()
+			if err != nil {
+				errors <- err
+				return
+			}
+
+			time.Sleep(pollInterval * time.Second)
+			fmt.Println("1")
+
+		}
+	}()
+
+	go func() {
+
+		for {
+
+			err := metricsHandler.storage.ReportMetrics()
+			if err != nil {
+				errors <- err
+				return
+			}
+
+			time.Sleep(reportInterval * time.Second)
+			fmt.Println("2")
+
+		}
+	}()
+
+	select {
+	case err := <-errors:
+		panic(err)
+	}
+
+}
+
+type MetricsStorage struct {
+	Gauge         map[string]float64
+	Counter       map[string]int64
+	ServerAddress string
+}
+
+func (s *MetricsStorage) ReportMetrics() error {
+
+	for name, value := range s.Gauge {
+
+		url := fmt.Sprintf("%s/update/%s/%s/%f", s.ServerAddress, "gauge", name, value)
+		resp, err := http.Post(url, "text/plain", nil)
+		if err != nil {
+			return err
+		}
+
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("status code <> 200, url : %s", url)
+		}
+	}
+
+	for name, value := range s.Counter {
+
+		url := fmt.Sprintf("%s/update1/%s/%s/%d", s.ServerAddress, "counter", name, value)
+		resp, err := http.Post(url, "text/plain", nil)
+		if err != nil {
+			return err
+		}
+
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("status code <> 200, = %d, url : %s", resp.StatusCode, url)
+		}
+	}
+
+	return nil
+
+}
+
+func (s *MetricsStorage) UpdateMetrics() error {
+
+	var memStats runtime.MemStats
+	runtime.ReadMemStats(&memStats)
+
+	v := reflect.ValueOf(memStats)
+
+	typeOfS := v.Type()
+
+	for i := 0; i < v.NumField(); i++ {
+		for _, metricsName := range metricsNames {
+			if metricsName != typeOfS.Field(i).Name {
+				continue
+			}
+
+			switch typeName := typeOfS.Field(i).Type.Name(); typeName {
+
+			case "uint64":
+				{
+					s.Gauge[metricsName] = float64(v.Field(i).Interface().(uint64))
+				}
+			case "uint32":
+				{
+					s.Gauge[metricsName] = float64(v.Field(i).Interface().(uint32))
+				}
+			case "float64":
+				{
+					s.Gauge[metricsName] = v.Field(i).Interface().(float64)
+				}
+
+			default:
+				return fmt.Errorf("unexpected type %s for metric %s", typeName, metricsName)
+			}
+
+		}
+	}
+
+	s.Counter["PollCount"]++
+
+	return nil
+}
--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -1,1 +1,110 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestMetricsHandler_UpdateMetric(t *testing.T) {
+
+	type want struct {
+		contentType string
+		statusCode  int
+		response    string
+		storage     MetricsUpdater
+	}
+
+	type test struct {
+		name    string
+		method  string
+		request string
+		storage MetricsUpdater
+		want    want
+	}
+
+	tests := []test{{
+		name:    "success empty",
+		method:  http.MethodPost,
+		request: "/update/gauge/someMetric/1.1",
+		storage: &MetricsStorage{
+			Gauge:   make(map[string]float64),
+			Counter: make(map[string]int64)},
+		want: want{
+			contentType: "text/plain",
+			statusCode:  200,
+			storage: &MetricsStorage{
+				Gauge:   map[string]float64{"someMetric": 1.1},
+				Counter: make(map[string]int64),
+			},
+		},
+	},
+		{
+			name:    "get",
+			method:  http.MethodGet,
+			request: "/update/gauge/someMetric/1.1",
+			storage: &MetricsStorage{
+				Gauge:   make(map[string]float64),
+				Counter: make(map[string]int64)},
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusMethodNotAllowed,
+				storage: &MetricsStorage{
+					Gauge:   make(map[string]float64),
+					Counter: make(map[string]int64),
+				},
+			},
+		},
+		{
+			name:    "value is bad",
+			method:  http.MethodPost,
+			request: "/update/gauge/someMetric/bad",
+			storage: &MetricsStorage{
+				Gauge:   make(map[string]float64),
+				Counter: make(map[string]int64)},
+			want: want{
+				contentType: "text/plain",
+				statusCode:  http.StatusBadRequest,
+				storage: &MetricsStorage{
+					Gauge:   make(map[string]float64),
+					Counter: make(map[string]int64),
+				},
+				response: "MetricsValue = bad, error: strconv.ParseFloat: parsing \"bad\": invalid syntax",
+			},
+		}}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(tt.method, tt.request, nil)
+			w := httptest.NewRecorder()
+			h := &MetricsHandler{
+				storage: tt.storage,
+			}
+			h.UpdateMetric(w, request)
+
+			result := w.Result()
+
+			defer result.Body.Close()
+			resBody, err := io.ReadAll(result.Body)
+			require.NoError(t, err)
+			err = result.Body.Close()
+			require.NoError(t, err)
+
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+
+			assert.Equal(t, tt.want.contentType, result.Header.Get("Content-Type"))
+			assert.Equal(t, tt.want.response, string(resBody))
+			//assert.InDeltaMapValues(t, tt.want.storage.(*MetricsStorage).Gauge, tt.storage.(*MetricsStorage).Gauge, 64)
+
+			eq := reflect.DeepEqual(tt.want.storage.(*MetricsStorage).Gauge, tt.storage.(*MetricsStorage).Gauge)
+			if !eq {
+				t.Error("MetricsStorage are unequal.")
+			}
+
+		})
+	}
+}
